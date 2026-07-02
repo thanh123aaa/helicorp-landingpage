e76977aae10999ebf54c69443631fd35809da860
thanh123aaa <tuyendung@helicorp.vn>
2026-07-02 18:45:13 +0700

    chore: fix useTracking syntax error

--- src/hooks/useTracking.ts
+++ src/hooks/useTracking.ts
@@ -2,7 +2,7 @@ import { useCallback } from 'react';
 
 export const useTracking = () => {
   const trackEvent = useCallback((action: string, category: string, label?: string) => {
-    console.log([Tracking Mock] Action: \, Category: \, Label: \);
+    console.log(`[Tracking Mock] Action: ${action}, Category: ${category}, Label: ${label}`);
   }, []);
 
   return { trackEvent };
